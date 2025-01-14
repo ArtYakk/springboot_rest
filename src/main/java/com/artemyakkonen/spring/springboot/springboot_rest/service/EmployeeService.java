72df2ba9e0446a064b1d0fac9b4bf6c04c646713
iakon <iakonien@gmail.com>
2025-01-14 22:28:19 +0300

    sss

---
+++ src/main/java/com/artemyakkonen/spring/springboot/springboot_rest/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.artemyakkonen.spring.springboot.springboot_rest.service;
+
+
+
+import com.artemyakkonen.spring.springboot.springboot_rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+//    public Employee saveEmployee(Employee employee);
+//    public Employee getEmployee(int id);
+//    public void deleteEmployee(int id);
+}
---
+++ src/main/java/com/artemyakkonen/spring/springboot/springboot_rest/service/EmployeeServiceImpl.java
@@ -0,0 +1,41 @@
+package com.artemyakkonen.spring.springboot.springboot_rest.service;
+
+
+import com.artemyakkonen.spring.springboot.springboot_rest.dao.EmployeeDAO;
+import com.artemyakkonen.spring.springboot.springboot_rest.entity.Employee;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+//    @Override
+//    @Transactional
+//    public Employee saveEmployee(Employee employee) {
+//        return employeeDAO.saveEmployee(employee);
+//    }
+//
+//    @Override
+//    @Transactional
+//    public Employee getEmployee(int id) {
+//        return employeeDAO.getEmployee(id);
+//    }
+//
+//    @Override
+//    @Transactional
+//    public void deleteEmployee(int id) {
+//        employeeDAO.deleteEmployee(id);
+//    }
+}
